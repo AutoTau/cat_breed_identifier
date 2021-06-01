64e58af2915c3b3d045ae3475e322a87c5362f34
AutoTau <zenbenjam@gmail.com>
2021-06-01 01:26:22 -0700

    Finished splash screen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,4 +1,6 @@
+import 'package:cat_breed_identifier/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 class MySplash extends StatefulWidget {
 
@@ -9,6 +11,23 @@ class MySplash extends StatefulWidget {
 class _MySplashState extends State<MySplash> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SplashScreenView(
+      navigateRoute: MyHomePage(),
+      duration: 5000,
+      imageSize: 160,
+      imageSrc: "images/icon.jpg",
+      text: "Cat Breed Identifier",
+      textType: TextType.ColorizeAnimationText,
+      textStyle: TextStyle(
+        fontSize: 40.0,
+      ),
+      colors: [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+      backgroundColor: Colors.white,
+    );
   }
 }
